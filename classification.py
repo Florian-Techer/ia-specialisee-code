ad0bc204dcd4fae6068543ab530aacd93d4f2301
Techer Florian <techerflorian1@gmail.com>
2025-03-04 11:51:48 +0100

    classification.py 2

--- classification.py
+++ classification.py
@@ -2,11 +2,9 @@ import numpy as np
 from PIL import Image
 import cv2
 # TensorFlow and tf.keras
-from tensorflow.keras.applications.imagenet_utils import
-preprocess_input, decode_predictions
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import
-MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 
 # Modèle ImageNet
 model = MobileNetV2(weights='imagenet')
